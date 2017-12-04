25e710d9a9a04234ee0f8ee6993082294db1857b
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-12-05 00:08:13 +0100

    Checking URL improvements

--- manga-dl.py
+++ manga-dl.py
@@ -1,15 +1,33 @@
-from lib import submanga as sm
 import sys
+from urllib.parse import urlparse
+from lib import submanga as sm
 
 VERSION = "0.1"
+
+def main():
+    uri    = sys.argv[1]
+    site   = urlparse(uri).netloc
+    domain = site.split('.')[0]
+
+    # Add here more sites!
+    
+    websites = {
+        'submanga': sm.getChapter
+    }
+    try:
+        websites[domain](uri)
+    except AttributeError:
+        print("Error: '{}' is not a valid URL!".format(uri))
+    except KeyError:
+        print("Ooops! Sorry, '{}' is not a valid URL or is not supported yet.".format(site))
 
 if __name__ == '__main__':
     try:
         if len(sys.argv) > 1:
-            sm.getChapter(sys.argv[1])
+            main()
         else:
             print("\nWelcome to manga-dl {}. Download manga from many websites!".format(VERSION))
             print("\nSUPPORTED WEBSITES\n\n - Submanga")
-            print("\nUSAGE\n\nmanga-dl [URL]\n")
+            print("\nUSAGE\n\n  manga-dl [URL]\n")
     except KeyboardInterrupt:
         print("\nCanceled. Goodbye!")
